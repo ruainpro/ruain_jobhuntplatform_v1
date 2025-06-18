e41649b5f1e8dfcb21fcd6fc5c91f394fdd0eec7
ruainpro <ruainpro@gmail.com>
2025-06-17 22:16:08 -0400

    done

--- src/main/java/com/dao/rjobhunt/Controller/authentication/UserController.java
+++ src/main/java/com/dao/rjobhunt/Controller/authentication/UserController.java
@@ -194,23 +194,6 @@ public class UserController {
         }
     }
 
-    @PostMapping("/logout")
-    public ResponseEntity<ApiResponse<String>> logout(HttpServletResponse response) {
-        ResponseCookie clearedCookie = ResponseCookie.from("jwt", "")
-            .httpOnly(true)
-            .secure(true)
-            .path("/")
-            .maxAge(0) // remove immediately
-            .build();
-
-        response.setHeader(HttpHeaders.SET_COOKIE, clearedCookie.toString());
-
-        return ResponseEntity.ok(ApiResponse.success("Logged out successfully", null));
-    }
-
-
-
-
 
     // Removed the role checks here as they are already managed in SecurityConfig
 
