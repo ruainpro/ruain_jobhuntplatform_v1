e990ced2242a8218be4640f7f9305c2c4f6479ad
ruainpro <ruainpro@gmail.com>
2025-06-25 22:51:12 -0400

    updating patch

--- src/main/java/com/dao/rjobhunt/Security/CorsGlobalConfig.java
+++ src/main/java/com/dao/rjobhunt/Security/CorsGlobalConfig.java
@@ -18,7 +18,7 @@ public class CorsGlobalConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:5173")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedMethods("GET", "POST", "PUT", "DELETE","PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
